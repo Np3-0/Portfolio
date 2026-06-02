ccbde8156351da9150286bd65622aa9a4ff51f03
Np3-0 <obnathan3@gmail.com>
2026-06-01 22:39:02 -0400

    about section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,36 @@
+import cardInfo from "../info/CardInfo.ts"
+
 interface CardProps {
-    title: string;
-    description: string;
+    index: number;
+    type?: string;
 }
 
-
-export default function Card({ title, description }: CardProps) {
+export default function Card({ index, type="N/A" }: CardProps) {
+    const {title, paragraph, details, subtitle, skills} = cardInfo[index];
     return (
-        <div className="bg-prussian block max-w-lg p-6 rounded-lg shadow-xs">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8 text-alabaster">{title}</h5>
-            <p className="text-body mb-6 text-alabaster font-semibold">{description}</p>
+        <div className="bg-prussian block max-w-5xl w-3xl p-6 rounded-lg shadow-xs">
+            <h2 className="mb-6 text-4xl font-bold tracking-tight text-heading leading-8 text-blue-500">{title}</h2>
+            {type === "Experience" && (
+                <>
+                    <p className="text-lg mb-3 text-alabaster font-semibold"><span className="rounded-full inline-block mr-2 bg-red-500 w-4 h-4"></span>{paragraph}</p>
+                    <p className="text-lg mb-6 ml-6 text-alabaster font-semibold"><li>{details}</li></p>
+                    <h2 className="text-2xl font-bold text-heading leading-4 text-blue-500"> {subtitle}:</h2>
+                    <div className="mt-4 flex flex-wrap gap-2">
+                        {skills?.map((skill, index) => (
+                            <span key={index} className="inline-flex items-center bg-dusk/75 border border-dusk text-alabaster text-md font-medium px-2 py-1 rounded">
+                                <span className="inline-block h-2 w-2 bg-alabaster rounded-full mr-2"></span>
+                                {skill}
+                            </span>
+                        ))}
+                    </div>
+                </>
+            )}
+            {type !== "Experience" && (
+                <>
+                    <p className="text-lg mb-3 text-alabaster font-semibold">{paragraph}</p>
+                    <p className="text-lg mb-6 text-alabaster font-semibold">{details}</p>
+                </>
+            )}
         </div>
     );
 }
---
+++ src/info/CardInfo.ts
@@ -0,0 +1,16 @@
+const cardInfo = [
+    {
+        title: "Info",
+        paragraph: "I am a rising senior at MCST who is interested in all areas of computer science, but Cybersecurity and Software Engineering are my main interests.",
+        details: "I have years of experience in creating projects, and have worked with groups such as NASA, FIRST, and more."
+    },
+    {
+        title: "Experience",
+        paragraph: "Employment: Intern @ Talon Biomarkers",
+        details: "Responsible for creating an automatic inventory system for dyes used in cytometric analysis.",
+        subtitle: "Skills",
+        skills: ["Python", "Java", "Firebase", "C#", "Unity", "CAD", "KiCAD", "Web Development"]
+    }
+];
+
+export default cardInfo;
